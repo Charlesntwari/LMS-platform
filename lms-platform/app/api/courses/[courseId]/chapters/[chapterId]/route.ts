5c26d3dc07b86176b4bcf92f7eb54d9919c54a17
charlesntwari <charlesntwari2@gmail.com>
2024-02-24 20:28:33 +0200

    ft-add chapter video

--- lms-platform/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ lms-platform/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,14 @@
+import Mux from "@mux/mux-node"
 import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 import { db } from "@/lib/db";
+
+const { Video } = new Mux{
+    process.env.MUX_TOKEN_ID!,
+    process.env.MUX_TOKEN_SECRET!
+    
+}
 
 export async function PATCH(
     req:Request,
@@ -34,6 +41,36 @@ export async function PATCH(
                 ...values
             }
         })
+
+        if(values.videoUrl){
+            const exsitingMuxData= await db.muxData.findFirst({
+                where:{
+                    chapterId: params.chapterId
+                }
+            })
+
+            if(exsitingMuxData){
+                await Video.Assets.del(exsitingMuxData.assetId)
+                await db.muxData.delete({
+                    where:{
+                        id: exsitingMuxData.id
+                    }
+                })
+            }
+            const asset= await Video.Assets.create({
+                input: values.videoUrl,
+                playback_policy: "public",
+                test: false
+            })
+
+            await db.muxData.create({
+                data:{
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id
+                }
+            })
+        }
 
         return NextResponse.json(Chapter)
 
