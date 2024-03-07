3e28db9eaf644612003c58013acc90c55e52218b
charlesntwari <charlesntwari2@gmail.com>
2024-03-07 13:17:58 +0200

    ft-add categories buttons

---
+++ lms-platform/app/(dashbord)/(routes)/search/_components/category-item.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils"
+import { IconType } from "react-icons"
+
+interface CategoryItemProps {
+    label: string
+    icon: IconType
+    value: string
+}
+
+export const CategoryItem = ({
+    label,
+    icon: Icon,
+    value
+}: CategoryItemProps) =>{
+    return(
+        <button
+            className={cn("py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",)}
+            type="button"
+        >
+            {Icon && <Icon size={20} />}
+            <div className="truncate">
+                {label}
+            </div>
+        </button>
+    )
+}
--- lms-platform/app/(dashbord)/(routes)/search/page.tsx
+++ lms-platform/app/(dashbord)/(routes)/search/page.tsx
@@ -1,6 +1,18 @@
-const SearchPage = () => {
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
+
+const SearchPage = async () => {
+    const categories = await db.category.findMany({
+        orderBy:{
+            name: "asc"
+        }
+    })
     return ( 
-        <div>this is search page</div>
+        <div className="p-6">
+            <Categories
+                items = {categories}
+            />
+        </div>
      );
 }
  
